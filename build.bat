d10c7d2113425611db3505a1c36eabf8b28ff3d6
czldl <czldl@tom.com>
2021-10-26 11:05:55 +0800

    jarsigner

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ jar cvfe ../atmgitops.jar atm.ATMMain atm/*.class atm/physical/*.class atm/trans
 cd ..
 keytool -genkey -alias mykey -keystore mykeystore.store -keypass mykeypass -storepass mystorepass  -validity 365  -dname "CN=liudongliang, OU=chzu, L=xxxy, S=chuzhou, O=anhui, C=CH"
 keytool -export -keystore mykeystore.store -alias mykey -validity 365 -file mykeystore.cert -storepass mystorepass
-rsigner -keystore myKeystore.store atmgitops.jar mykey -storepass mystorepass -keypass mykeypass
+jarsigner -keystore myKeystore.store atmgitops.jar mykey -storepass mystorepass -keypass mykeypass
 echo keystore "file:myKeystore.store","JKS"; grant signedBy "mykey" { permission java.io.FilePermission"<<ALL FILES>>","read";};>myKeystore.policy
 rd /S/Q target
 java -jar atmgitops.jar
